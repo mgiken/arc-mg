fa48aa7570967d3cff9584451118fd56e2fd9b6c
Ryuji Matsumura <ryuji@mgiken.com>
2011-06-28 06:29:47 +0900

    update Makefile

--- bin/arc.sh
+++ bin/arc.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 export ARCVER="0"
-export ARCLIB="$(cd $(dirname $(readlink -f $0))/.. && pwd)/lib"
+export PREFIX="$(cd $(dirname $(readlink -f $0))/.. && pwd)"
+export ARCLIB="$PREFIX/lib"
+
+if [ "$1" = "--show-prefix" ]; then
+  echo $PREFIX
+  exit 0
+fi
 
 if [ $# -eq 0 -a "$(type rlwrap)" ]; then
   rl='rlwrap -c -r -q "\"" -b "[]()!:~\"" -C arc'
